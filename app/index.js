ce0cd7846d8d288569afc29ce088c1a9ae4dc9f4
ASteinheiser <asteinhe@asu.edu>
2016-05-09 14:28:01 -0700

    setup everything

---
+++ app/index.js
@@ -0,0 +1,42 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  askFor: function() {
+    var self = this;
+    var done = this.async();
+    var prompts = [
+      {
+        name: 'zooidname',
+        message: 'What would you like your zooid to be called?',
+        'default': 'zooid-sample'
+      },
+      {
+        name: 'author',
+        message: 'Who is the author?'
+      },
+      {
+        name: 'githubUser',
+        message: 'What is your github username?'
+      }
+    ];
+
+    this.prompt(prompts, function(props) {
+      self.zooidname = props.zooidname;
+      self.author = props.author;
+      self.githubUrl = "https://github.com/" + props.githubUser;
+      return done();
+    })
+  },
+  projectFiles: function() {
+    this.template('src/_zooid.css', 'src/' + self.zooidname + '.css');
+    this.template('src/_zooid.js', 'src/' + self.zooidname + '.js');
+    this.template('src/_zooid.spec.js', 'src/' + self.zooidname + '.spec.js');
+    this.template('src/_index.js', 'src/index.js');
+    this.template('test/_setup.js', 'test/.setup.js');
+    this.template('test/_mocha.opts', 'test/mocha.opts');
+    this.template('_package.json', 'package.json');
+    this.template('_babelrc', '.babelrc');
+    this.template('_webpack.config.js', 'webpack.config.js');
+    return this.template('_gitignore', '.gitignore');
+  }
+});
