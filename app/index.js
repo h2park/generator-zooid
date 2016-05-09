76a8d6581c351b9f32f00e4c420701a4c90cc776
ASteinheiser <asteinhe@asu.edu>
2016-05-09 16:57:20 -0700

    finished

--- app/index.js
+++ app/index.js
@@ -1,46 +1,51 @@
 var generators = require('yeoman-generator');
+var kebabCase = require('kebab-case');
 
 module.exports = generators.Base.extend({
 
   prompting: function () {
     var self = this;
-    var done = this.async();
-    return this.prompt(
+    var prompts = [
       {
         type: 'input',
         name: 'zooidname',
-        message: 'What would you like your zooid to be called? (if you want zooid-ui-spinner, enter \"Spinner\")',
-        'default': 'Zooid'
-      },
-      {
+        message: 'What would you like your zooid to be called? (if you want zooid-ui-page-header, enter \"PageHeader\")',
+        default: 'Zooid'
+      },{
         type : 'input',
         name: 'author',
         message: 'Who is the author?',
-        'default': 'Octoblu Inc'
+        default: 'Octoblu Inc'
       },{
         type: 'input',
         name: 'githubUser',
         message: 'What is your github username?',
-        'default': 'octoblu'
+        default: 'octoblu'
       }
-    ).then(function (answers) {
+    ];
+    return this.prompt(prompts).then(function (answers) {
       self.zooidname = answers.zooidname;
-      self.zooidnamekebab = 'zooid-ui-' + answers.zooidname.toLowerCase();
+      self.zooidnamekebab = 'zooid-ui' + kebabCase(answers.zooidname);
       self.author = answers.author;
       self.githubUrl = "https://github.com/" + answers.githubUser;
-      return done();
-    }.bind(this));
+    });
   },
-  projectFiles: function() {
-    this.template('src/_zooid.css', 'src/' + self.zooidname + '.css');
-    this.template('src/_zooid.js', 'src/' + self.zooidname + '.js');
-    this.template('src/_zooid.spec.js', 'src/' + self.zooidname + '.spec.js');
-    this.template('src/_index.js', 'src/index.js');
-    this.template('test/_setup.js', 'test/.setup.js');
-    this.template('test/_mocha.opts', 'test/mocha.opts');
-    this.template('_package.json', 'package.json');
-    this.template('_babelrc', '.babelrc');
-    this.template('_webpack.config.js', 'webpack.config.js');
-    return this.template('_gitignore', '.gitignore');
+  writing: function() {
+    var context = {
+      zooidname: this.zooidname,
+      zooidnamekebab: this.zooidnamekebab,
+      author: this.author,
+      githubUrl: this.githubUrl
+    }
+    this.template('src/_zooid.css', 'src/' + this.zooidname + '.css', context);
+    this.template('src/_zooid.js', 'src/' + this.zooidname + '.js', context);
+    this.template('src/_zooid.spec.js', 'src/' + this.zooidname + '.spec.js', context);
+    this.template('src/_index.js', 'src/index.js', context);
+    this.template('test/_setup.js', 'test/.setup.js', context);
+    this.template('test/_mocha.opts', 'test/mocha.opts', context);
+    this.template('_package.json', 'package.json', context);
+    this.template('_babelrc', '.babelrc', context);
+    this.template('_webpack.config.js', 'webpack.config.js', context);
+    this.template('_gitignore', '.gitignore', context);
   }
 });
