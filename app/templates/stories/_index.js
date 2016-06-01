cdf5f96b9d13799e19ba26bf8aceb403065549c7
ASteinheiser <asteinhe@asu.edu>
2016-05-31 18:28:00 -0700

    actually adding story book

---
+++ app/templates/stories/_index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+
+import <%= zooidname %> from '../src';
+
+storiesOf('<%= zooidname %>', module)
+  .add('Basic', () => (
+    <<%= zooidname %>/>
+  ));
