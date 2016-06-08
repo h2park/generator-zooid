bdb8b4f689dae17a7eea51ddd782b4ff13a032f3
Olu Ayandosu <oayandosu@gmail.com>
2016-06-08 12:13:14 -0700

    v2.1.0 Add spec template

---
+++ app/templates/src/_zooid.spec.js
@@ -0,0 +1,11 @@
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import React from 'react';
+import sinon from 'sinon';
+import { mount, shallow } from 'enzyme';
+
+import <%= zooidname %> from './';
+
+chai.use(chaiEnzyme());
+
+describe('<<%= zooidname %> />', () => {});
